4b1ea6daaa4c9ada8a1e6b835bb40af83dfc7995
boffin311 <himanshu31198@gmail.com>
2019-04-20 18:36:02 +0530

    Database configured

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,3 @@
 const mongoose=require('mongoose')
-const mongooseConnection=mongoose.connect('mongodb+srv://Himanshu:HZyxVOS17lNJyrlU@firstcluster-bhxab.mongodb.net/postit',{useNewUrlParser:true})
+const mongooseConnection=mongoose.connect('mongodb+srv://Himanshu:HZyxVOS17lNJyrlU@firstcluster-bhxab.mongodb.net/postit?retryWrites=true',{useNewUrlParser:true})
 module.exports=mongooseConnection;
