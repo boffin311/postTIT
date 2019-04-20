f77ef99e2c0eddf3646fe75b077cd03c130d0ae2
boffin311 <himanshu31198@gmail.com>
2019-04-20 16:34:39 +0530

    The first upload with basic functionality

--- controller/allPostController.js
+++ controller/allPostController.js
@@ -2,7 +2,13 @@ const Posts=require('../models/postModel')
 module.exports.getAllPosts=(req,res,next)=>{
     Posts.find().then((result) => {
      console.log(result)
+     if(req.path=='/'){
      res.render('allPost',{isAuthenticated:req.session.isLoggedIn,allPosts:result})  
+     }
+     else{
+         res.json(result)
+     }
+    res.json(result)
     }).catch((err) => {
         console.log(err)
     });
--- routes/allPostRoutes.js
+++ routes/allPostRoutes.js
@@ -2,4 +2,5 @@ const express=require('express')
 const routes=express.Router();
 const allPostcontroller=require('../controller/allPostController')
 routes.get('/',allPostcontroller.getAllPosts)
+routes.get('/getall/posts',allPostcontroller.getAllPosts)
 module.exports=routes
